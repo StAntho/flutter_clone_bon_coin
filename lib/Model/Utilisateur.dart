e19d07be86b2a0c521cd7661ecf20993f743b2ba
Anthony <anthony.stanix@gmail.com>
2022-06-15 23:15:11 +0200

    add annonce collection to user class

--- lib/Model/Utilisateur.dart
+++ lib/Model/Utilisateur.dart
@@ -1,73 +1,63 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 // Constructeur de la classe utilisateur via Firebase
 
 class Utilisateur {
   //Attributs
-  late String id ;
+  late String id;
   late String nom;
   late String prenom;
   late String mail;
   String? avatar;
   String? pseudo;
-  DateTime birthday =  DateTime.now();
-
-
-
-
+  DateTime birthday = DateTime.now();
+  List? annonces;
 
   //Constructeur
-  Utilisateur(DocumentSnapshot snapshot){
+  Utilisateur(DocumentSnapshot snapshot) {
     String? provisoire;
     id = snapshot.id;
-    Map<String,dynamic> map = snapshot.data() as Map<String,dynamic>;
+    Map<String, dynamic> map = snapshot.data() as Map<String, dynamic>;
     nom = map["NOM"];
     prenom = map["PRENOM"];
     mail = map["MAIL"];
     provisoire = map["AVATAR"];
-    if(provisoire == null){
+    if (provisoire == null) {
       // Une image spécifique que je vais luis donner
-      avatar = "https://firebasestorage.googleapis.com/v0/b/ipssia3bdfirstapplication.appspot.com/o/icon.png?alt=media&token=c3d7cb1c-1d44-4bca-aeb8-63f08d559926";
+      avatar =
+          "https://firebasestorage.googleapis.com/v0/b/ipssia3bdfirstapplication.appspot.com/o/icon.png?alt=media&token=c3d7cb1c-1d44-4bca-aeb8-63f08d559926";
+    } else {
+      avatar = provisoire;
     }
-    else
-      {
-        avatar = provisoire;
-      }
     provisoire = map["PSEUDO"];
-    if(provisoire == null){
-
+    if (provisoire == null) {
       pseudo = "";
-    }
-    else
-    {
+    } else {
       pseudo = provisoire;
     }
     Timestamp timestamp = map["BIRTHDAY"];
     birthday = timestamp.toDate();
-
+    provisoire = map["ANNONCES"];
+    if (provisoire == null) {
+      annonces = [];
+    } else {
+      annonces = [provisoire];
+    }
   }
 
   //Deuxième constructeur qui affecter les valeurs à vide
-Utilisateur.empty(){
+  Utilisateur.empty() {
     id = "";
-    nom ="";
-    prenom="";
-    mail ="";
-    avatar ="";
-    pseudo ="";
+    nom = "";
+    prenom = "";
+    mail = "";
+    avatar = "";
+    pseudo = "";
     birthday = DateTime.now();
-}
+    annonces = [];
+  }
 
-
-String nomComplet(){
+  String nomComplet() {
     return prenom + " " + nom;
-}
-
-
-
-
-
-
-
+  }
 }
